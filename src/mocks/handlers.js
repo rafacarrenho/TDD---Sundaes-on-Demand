6024c73158326fe2a516da7f0f85dc988e5cc5d6
rafacarrenho <rafael.carrenho@hotmail.com>
2021-04-06 22:14:43 -0300

    All others tests and change to Yarn

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,23 +1,24 @@
-import { rest } from "msw";
-
-const url = "http://localhost:3030";
+import { rest } from 'msw';
 
 export const handlers = [
-  rest.get(`${url}/scoops`, (req, res, ctx) => {
+  rest.get('http://localhost:3030/scoops', (req, res, ctx) => {
     return res(
       ctx.json([
-        { name: "Chocolate", imagePath: "/images/chocolate.png" },
-        { name: "Vanilla", imagePath: "/images/vanilla.png" },
+        { name: 'Chocolate', imagePath: '/images/chocolate.png' },
+        { name: 'Vanilla', imagePath: '/images/vanilla.png' },
       ])
     );
   }),
-  rest.get(`${url}/toppings`, (req, res, ctx) => {
+  rest.get('http://localhost:3030/toppings', (req, res, ctx) => {
     return res(
       ctx.json([
-        { name: "Cherries", imagePath: "/images/cherries.png" },
-        { name: "M&Ms", imagePath: "/images/m-and-ms.png" },
-        { name: "Hot fudge", imagePath: "/images/hot-fudge.png" },
+        { name: 'Cherries', imagePath: '/images/cherries.png' },
+        { name: 'M&Ms', imagePath: '/images/m-and-ms.png' },
+        { name: 'Hot fudge', imagePath: '/images/hot-fudge.png' },
       ])
     );
+  }),
+  rest.post('http://localhost:3030/order', (req, res, ctx) => {
+    return res(ctx.json({ orderNumber: 123455676 }));
   }),
 ];
